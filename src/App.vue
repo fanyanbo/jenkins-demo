00d8101fc6a12fa58bc6d36b3bddf82f35034294
fanyanbo <fanyanbo@coocaa.com>
2020-04-22 19:46:59 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home33</router-link> |
+      <router-link to="/">Home44</router-link> |
       <router-link to="/about">About22</router-link>
     </div>
     <router-view/>
